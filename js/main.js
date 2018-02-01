4402ad72eb989ecc5e95cd96b1412f3b38587b12
Scott Pierce <scott.pierce@centro.net>
2018-02-01 06:34:27 -0600

    Initial version

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(".p2").typed({
+    strings: ["FAMILY", "COWORKERS", "FRIENDS", "SITES"],
+    typeSpeed: 80,
+    backSpeed: 10,
+    backDelay: 1800,
+    showCursor: false,
+    loop: false
+  });
+});
